571c73548d16e8420c0ce598d8aa0cca012b9e34
gaverin <gabriele.averin@outlook.com>
2026-04-10 13:11:31 +0200

    initialized training  folder with uv

---
+++ training/main.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+import keras
+from keras.models import Sequential
+from keras.layers import Input, Conv1D, MaxPooling1D, Dropout, Flatten, Dense
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.optimizers import Adam
+
+"""
+320x200 RGB565 images
+"""
+
+
+
+
+
+if __name__ == "__main__":
+    main()
